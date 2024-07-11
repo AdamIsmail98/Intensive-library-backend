16d5e8df9cef4281eafb5137e5bfad571c2d42c9
Adam Ismail <adam_ismaail98@outlook.com>
2024-07-11 22:41:18 +0200

    added user and auth to add and sign in users

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import categories from "./routes/categories";
 import libraryItems from "./routes/libraryItems";
+import users from "./routes/users";
+import auth from "./routes/auth";
 import cors from "cors";
 
 const app = express();
@@ -9,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/categories", categories);
 app.use("/api/libraryitems", libraryItems);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const PORT = 5544;
 
